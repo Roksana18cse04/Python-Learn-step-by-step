264c0bbba4274cdc050f41205e6989739e120ebe
roksana <roksana18cse04@gmail.com>
2023-08-20 23:06:42 +0600

    math related function

---
+++ Mathrelated.py
@@ -0,0 +1,24 @@
+from math import*
+
+'''
+base = float(input("Enter Your base : "))
+height =float(input("Enter Your height "))
+
+area = 0.5*base*height
+print(area)
+'''
+# math related function{max(),abs(),min()}
+# math related function use math Library import
+print(max(10,30))
+print(abs(-20))
+print(min(10,30))
+
+print(pow(3,2))  # power is 2 
+
+print(sqrt(25))
+
+
+print(round(3.56))
+
+print(floor(3.45))
+print(ceil(3.45))
