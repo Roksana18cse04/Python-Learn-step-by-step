e7a5f779f9a9e332811d9e43e36201e91e5d6b5e
roksana <roksana18cse04@gmail.com>
2023-08-20 20:48:08 +0600

    Python Learn

---
+++ num_test.py
@@ -0,0 +1,12 @@
+print("Python in first Programe in Visual Code ....")
+
+'''
+Variable and data set up
+programe
+'''
+name="Roksana Akter"
+age=32
+gpa=3.47
+
+print("Our new employee is " +name) 
+print("She is currently ",age ,"Years old") # without String variable are use comma(,) exchange plus(+)
---
+++ numericalOP.py
@@ -0,0 +1,33 @@
+'''
+operator 
++,_,*,/,%
+Opreation
+'''
+print(11+4)
+print(11-4)
+print(11*4)
+print(11/4)
+print(11%4)
+
+#variable declare
+a=42
+b=3
+print(a+b)  #Plus operator for sum
+print(a-b) #minus operator for sub
+print(a*b) #Multiplication operator for product
+print(a/b) #Division operator for divided
+print(a%b) #Module operator for reminder in division
+
+print(3*2) #product
+
+'''
+
+# double * means sThe syntax ** 
+in Python represents the exponentiation operation
+, output will be 9. Explanation: The code print(3 ** 2)
+ is a Python statement that calculates the result of raising 
+ the number 3 to the power of 2, which is equivalent to 3 multiplied 
+ by itself twice
+
+'''
+print(3**2)
