1255cf2f3f3ec0d7c2fc6bb3924e8b86785375f8
roksana <roksana18cse04@gmail.com>
2023-08-20 21:33:45 +0600

    Type casting here

---
+++ typeCasting.py
@@ -0,0 +1,20 @@
+num1=input("Enter Ypur First Number : ")
+num2=input("Enter Ypur second Number : ")
+'''
+here code is concate of string because
+two number is string and after adde two number
+then is together
+'''
+sum =num1+num2
+print("num1 + num2 = "+sum)
+
+'''
+similar way type casting below there
+code 
+int(input("somthing")) here data type is int, double, float,long
+'''
+
+num3 = int(input("Enter Num3 is : "))
+num4 = int(input("Enter Num4 is : "))
+sum = num3+num4
+print("Num3 + num4 = ",sum)
