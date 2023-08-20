0ba908a13384ef0d528fc610bc6a59ecb61dc3c2
roksana <roksana18cse04@gmail.com>
2023-08-21 00:38:05 +0600

    RelatinalOP

---
+++ dataTypeShow.py
@@ -0,0 +1,21 @@
+num=12
+n1=12.3
+name="Roksana Akter" #String type
+ch='a'
+print(type(num))
+print(type(n1))
+print(type(name))
+print(type(ch))
+
+#string formated
+print(f"{num} + {n1} = {num+n1}")
+print("Roksana Akter " , end=" ")  # [end = " "] use no new line print and space print
+print("0130######6")
+
+#relation operatoe and boolean operation
+print(num<n1)
+print(num>n1)
+print(num<=n1)
+print(num>=n1)
+print(num==n1)
+print(num!=n1)
