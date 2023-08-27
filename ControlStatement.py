de08c67de7ab4a37af6198af27a035a9063e784f
roksana <roksana18cse04@gmail.com>
2023-08-27 13:01:55 +0600

    control

---
+++ ControlStatement.py
@@ -0,0 +1,28 @@
+'''
+num=70
+
+if num>=40:
+    print("Pass")
+if num<40:
+    print("Fail")
+
+if num%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+'''
+# big number in three nums
+
+a=input()
+b=input()
+c=input()
+
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b>c:
+        print(b)
