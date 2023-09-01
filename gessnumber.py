cc591aeb9e0b67681bc0d293a5abb44c41aac276
roksana <roksana18cse04@gmail.com>
2023-09-01 10:28:45 +0600

    g

--- gessnumber.py
+++ gessnumber.py
@@ -2,9 +2,7 @@ import random
 ### gauss number
 for x in range(1,6):
     geussNumber=input("Enter Your gauss between 1 to 5 : ")
-
     randomNumber = random.randint(1,5)  # random import 
-
     if geussNumber == randomNumber:
         print("You Have Win")
     else:
